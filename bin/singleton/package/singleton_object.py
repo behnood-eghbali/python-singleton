3622a20a2e6c4c754d778a26b6acf42eb9c20df8
Behnood Eghbali <behnood-eghbali@users.noreply.github.com>
2020-12-13 22:41:00 +0330

    Update singleton_object.py

--- bin/singleton/package/singleton_object.py
+++ bin/singleton/package/singleton_object.py
@@ -1,11 +1,12 @@
+# implementing singleton pattern with magic methods (operator overloading methods)
 class SingletonObject(object):
 
     class __SingletonObject():
 
-        def __init__(self): #constructor
+        def __init__(self):
             self.val = None
             
-        def __str__(self): # operator overloading method
+        def __str__(self):
             return "{0!r} {1}".format(self, self.val)
 
     instance = None
