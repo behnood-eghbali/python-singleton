35272462ebff7724ec540e81071a292b6a70938d
Behnood Eghbali <behnood-eghbali@users.noreply.github.com>
2020-12-13 22:28:03 +0330

    Update singleton_object.py

--- bin/singleton/package/singleton_object.py
+++ bin/singleton/package/singleton_object.py
@@ -2,21 +2,21 @@ class SingletonObject(object):
 
     class __SingletonObject():
 
-        def __init__(self):
+        def __init__(self): #constructor
             self.val = None
-        
-        def __str__(self):
+            
+        def __str__(self): # operator overloading method
             return "{0!r} {1}".format(self, self.val)
 
-        instance = None
+    instance = None
 
-        def __new__(cls):
-            if not SingletonObject.instance:
-                SingletonObject.instance = SingletonObject.__SingletonObject()
-            return SingletonObject.instance
+    def __new__(cls):
+        if not SingletonObject.instance:
+            SingletonObject.instance = SingletonObject.__SingletonObject()
+        return SingletonObject.instance
 
-        def __getattr__(self, name):
-            return getattr(self.instance, name)
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
 
-        def __setattr__(self, name):
-            return setattr(self.instance, name)
+    def __setattr__(self, name):
+        return setattr(self.instance, name)
