1834345bb31857fb8fb99f201dcaa91638d97abc
Behnood Eghbali <behnood-eghbali@users.noreply.github.com>
2020-12-21 18:18:38 +0330

    Update singleton_logger.py

--- bin/singleton/singleton_logger.py
+++ bin/singleton/singleton_logger.py
@@ -1,16 +1,1 @@
-import os
-from singleton_object import SingletonObject
 
-log_path = os.chdir(os.path.expanduser('singleton/bin/data/data.log'))
-
-logger_object = SingletonObject(log_path)
-
-logger_object.critical("CRITICAL")
-
-logger_object.error("ERROR")
-
-logger_object.warn("WARN")
-
-logger_object.info("INFO")
-
-logger_object.debug("DEBUG")
